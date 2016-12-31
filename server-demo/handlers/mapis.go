77b4dd6cdc8e549162ef87eba4eab08c98da2441
程二枫 <369495368@qq.com>
2017-01-01 04:56:56 +0800

    no message

---
+++ server-demo/handlers/mapis.go
@@ -0,0 +1,23 @@
+package handlers
+
+import (
+	"fmt"
+	"gopkg.in/gin-gonic/gin.v1"
+	"net/http"
+	log "github.com/brasbug/log4go"
+
+)
+
+
+
+
+func GetHandler(c *gin.Context) {
+	value, exist := c.GetQuery("key")
+	if !exist {
+		value = "the key is not exist!"
+	}
+
+	log.Info(value)
+	c.Data(http.StatusOK, "text/plain", []byte(fmt.Sprintf("get success! %s\n", value)))
+	return
+}
--- server-demo/main.go
+++ server-demo/main.go
@@ -2,13 +2,34 @@ package main
 
 import (
 	"gopkg.in/gin-gonic/gin.v1"
-
+	log "github.com/brasbug/log4go"
+	"Project/server-demo/handlers"
 )
 
-func main()  {
+func SetLog() {
+	w := log.NewFileWriter()
+	w.SetPathPattern("./log/log-%Y%M%D.log")
+	c := log.NewConsoleWriter()
+	c.SetColor(true)
+	log.Register(w)
+	log.Register(c)
+	log.SetLevel(log.DEBUG)
+	log.SetLayout("2006-01-02 15:04:05")
+}
+
+func main() {
+	SetLog()
+	defer log.Close()
 	gin.SetMode(gin.DebugMode)
-	r := gin.New()
+	router := gin.Default()
+	router.Use(Middleware)
+	router.GET("/get", handlers.GetHandler)
+	router.Run(":8081")
+}
 
-
-
+func Middleware(c *gin.Context) {
+	if c.Request.Form == nil {
+		c.Request.ParseMultipartForm(32 << 20)
+	}
+	log.Info("%s,%s%s",c.Request.Form,c.Request.Host,c.Request.RequestURI)
 }
