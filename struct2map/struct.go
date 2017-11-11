15a3e7f2e743b46cb702c69106239b71562b79bc
flywithbug <flywithme.top@gmail.com>
2017-11-12 02:31:00 +0800

    no message

--- struct2map/struct.go
+++ struct2map/struct.go
@@ -1,1 +1,32 @@
-package struct2map
+package main
+
+import (
+	"github.com/fatih/structs"
+	//"net/http"
+
+	"fmt"
+
+)
+
+type Server struct {
+	Name        string `json:"name"`
+	ID          int
+	Enabled     bool
+	User       string
+}
+
+
+func main()  {
+
+
+	server := &Server{
+		Name:    "gopher",
+		ID:      123456,
+		Enabled: true,
+		User: "a9090909",
+	}
+
+	fmt.Println(structs.Map(server))
+
+
+}
